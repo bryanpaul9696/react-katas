eddd5502669277c41bf7649a47cbf0f923efc5f7
bryanpaul9696 <gduran9696@gmail.com>
2023-10-26 13:35:20 +0200

    [BGD] Fix : Refactor quote number

--- src/06-apiFetch/breakingBadApi/BreakingBadQuotes.tsx
+++ src/06-apiFetch/breakingBadApi/BreakingBadQuotes.tsx
@@ -2,12 +2,12 @@ import {useFetchBreakingBadQuotes} from "./hooks/useFetchBreakingBadQuotes.ts";
 import {useState} from "react";
 
 export const BreakingBadQuotes = () => {
-    const [counter, setCounter] = useState(0);
-    const {data, isLoading} = useFetchBreakingBadQuotes(counter)
+    const [quoteNumber, setQuoteNumber] = useState(0);
+    const {data, isLoading} = useFetchBreakingBadQuotes(quoteNumber)
     const {author, quote} = !!data && data[0];
 
     const increment = () => {
-        setCounter(counter + 1)
+        setQuoteNumber(quoteNumber + 1)
     }
 
     return (
